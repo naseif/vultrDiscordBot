35da563af282226cb93cb4100983329011df3e9d
naseif <d2mds21@gmail.com>
2021-04-24 17:33:28 +0000

    removed template literal ... not needed here

--- src/vultrinitializer.js
+++ src/vultrinitializer.js
@@ -1,5 +1,5 @@
 const VultrNode = require("@vultr/vultr-node");
 const { vultrAPI } = require("../config.json");
-const api = VultrNode.initialize({ apiKey: `${vultrAPI}` });
+const api = VultrNode.initialize({ apiKey: vultrAPI });
 
 exports.api = api;
